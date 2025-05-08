97305a407efe878d0d2d454c16a905114d9a767d
Vanderlei Romera <vanderleiromera@gmail.com>
2025-05-08 11:15:54 -0500

    Melhorar consulta de produtos sem estoque para ser mais flexível

--- app/modules/example_pairs.py
+++ app/modules/example_pairs.py
@@ -192,15 +192,16 @@ JOIN
 JOIN
     product_template pt ON pp.product_tmpl_id = pt.id
 LEFT JOIN
-    stock_quant sq ON pp.id = sq.product_id AND sq.location_id = (SELECT id FROM stock_location WHERE name = 'Stock' LIMIT 1)
+    stock_quant sq ON pp.id = sq.product_id AND sq.location_id IN (SELECT id FROM stock_location WHERE usage = 'internal')
 JOIN
     sale_order so ON sol.order_id = so.id
 WHERE
-    so.date_order >= NOW() - INTERVAL '30 days'  -- Filtrando para os últimos 30 dias
+    so.date_order >= CURRENT_DATE - INTERVAL '30 days'  -- Filtrando para os últimos 30 dias
+    AND so.state IN ('sale', 'done')  -- Apenas pedidos confirmados ou concluídos
 GROUP BY
     pt.id, pt.name, pt.default_code
-HAVING SUM
-    (sol.product_uom_qty) > 0 AND COALESCE(SUM(sq.quantity), 0) = 0;
+HAVING
+    SUM(sol.product_uom_qty) > 0 AND COALESCE(SUM(sq.quantity), 0) <= 0;
 """,
         },
         {
